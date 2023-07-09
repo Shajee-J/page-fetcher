61726035f27d06fa2c12420f7cee139b2f179403
Shajee-j <Shajeejm@gmail.com>
2023-07-09 17:36:09 -0400

    create local location and enabled terminal cmds to be passed in

--- fetcher.js
+++ fetcher.js
@@ -1,24 +1,23 @@
 const request = require('request');
 const fs = require('fs');
+const argv = process.argv.slice(2) // will take info entered into the terminal. 
 
-const content = body;
 
+request(argv[0], (error, response, body) => { // sends a request to the website specified in argv[0]
 
-request('http://www.google.com', (error, response, body) => {
+  fs.writeFile(argv[1], body, err => {
+    if (err) {
+      console.error(err);    // if no errors are found, will write info requested from argv into the local path specified in argv[1]
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to $${argv[1]}`) //loging how much is being downloaded and where.
 
-  console.log('error:', error); // Print the error if one occurred
+  });
+  console.log('error:', error);
 
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('statusCode:', response && response.statusCode);
 
-  console.log('body:', content); // Print the HTML for the Google homepage.
-
+  console.log('body:', body);
 });
 
 
 
-fs.writeFile('./Body.html', content, err => {
-  if (err) {
-    console.error(err);
-  }
-  // file written successfully
-});
